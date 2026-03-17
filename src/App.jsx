7f9f22f88199b1a10e72f788c12e08d593ea5539
Swetha <swetha2001as@gmail.com>
2026-03-17 16:22:03 +0400

    create login page and minitools section in dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Layout from './components/layouts/Layout';
 import AppRoutes from './Routes';
 import './index.css'
 
 function App() {
   return (
-    <Layout>
-      <AppRoutes />
-    </Layout>
+    <AppRoutes />
   );
 }
 
